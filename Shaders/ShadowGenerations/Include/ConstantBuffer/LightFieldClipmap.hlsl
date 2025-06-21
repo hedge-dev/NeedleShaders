eac12a3c6363c0cb1edfae61780a6c69e856dc9d
Justin113D <Justin113D@gmail.com>
2025-06-21 12:20:37 +0200

    Documented and improved deferred_ambient shader

--- Shaders/ShadowGenerations/Include/ConstantBuffer/LightFieldClipmap.hlsl
+++ Shaders/ShadowGenerations/Include/ConstantBuffer/LightFieldClipmap.hlsl
@@ -3,10 +3,25 @@
 
 cbuffer cbLightFieldClipmap : register(b10)
 {
-  float4 u_lf_param;
-  float4 u_probe_param[12];
-  float4 u_probe_resolution[12];
-  row_major float4x4 u_inv_obb[12];
+    // --- Light field parameter ---
+    // X: number of light fields
+    // Y: Distance (in pixels) from the surface (in the surfaces normal direction)
+    //    at which the lightfield probe should be sampled
+    // Z: Distance factor to the bounds of the lightfield at which the influence
+    //    should start (linearly) falling
+    // W: if not 0, ignore floor/bottom samples and use black instead
+    float4 u_lf_param;
+
+    // ???
+    float4 u_probe_param[12];
+
+    // --- Light field probe texture resolutions ---
+    // xyz: resolution of a single probe tile
+    // w: unused
+    float4 u_probe_resolution[12];
+
+    // Matrices to transform from world space to light field local space
+    row_major float4x4 u_inv_obb[12];
 }
 
 #endif
--- Shaders/ShadowGenerations/deferred_ambient_ps.ps.hlsl
+++ Shaders/ShadowGenerations/deferred_ambient_ps.ps.hlsl
@@ -20,7 +20,7 @@ Texture3D<float4> WithSampler(s_LightField9);
 Texture3D<float4> WithSampler(s_LightField10);
 Texture3D<float4> WithSampler(s_LightField11);
 
-void ComputeAmbientValue(uint index, float4 position, float3 normal, Texture3D<float4> lf_texture, SamplerState lf_sampler, inout float remainder, inout float buffer[6])
+void ComputeProbeSample(uint index, float4 position, float3 normal, Texture3D<float4> lf_texture, SamplerState lf_sampler, inout float remainder, inout float buffer[6])
 {
 	uint light_field_count = (uint)floor(0.5 + u_lf_param.x);
 	if(remainder <= 0.0 || index >= light_field_count)
@@ -31,42 +31,51 @@ void ComputeAmbientValue(uint index, float4 position, float3 normal, Texture3D<f
 	float4x4 lf_matrix = u_inv_obb[index];
 	float3 lf_position = mul(position, lf_matrix).xyz;
 
+	// getting the furthest distance to the light fields edges
 	float3 t = abs(lf_position);
-	float t2 = max(max(t.x, t.y), t.z) * 2;
+	float distance = max(max(t.x, t.y), t.z) * 2;
 
-	if(t2 > 1.0)
+	// if the point is outside the light field,
+	// then we can skip it
+	if(distance >= 1.0)
 	{
 		return;
 	}
 
-	float t3 = saturate(1.0 - u_lf_param.z);
-    float factor = saturate(1.0 - (1.0 / (1.0 - max(0.01, t3))) * (t2 - t3));
+	// getting the sampling influence; The point at which the influence
+	// starts falling (fading out) is controlled with u_lf_param.z
+	float distance_falloff_start = saturate(1.0 - u_lf_param.z);
+    float factor = saturate(1.0 - (1.0 / (1.0 - max(0.01, distance_falloff_start))) * (distance - distance_falloff_start));
 
-	if(factor <= 0)
-	{
-		return;
-	}
+	// Note: There was originally an if check here that checks if factor is above 0,
+	// but it can only be 0 when distance is >= 1.0, which we rule out before already
 
 	float3 lf_normal = normalize(mul(normal, (float3x3)lf_matrix));
 	float3 probe_resolution = u_probe_resolution[index].xyz;
 
-	float3 sample_pos = clamp(
-		saturate(lf_position + 0.5) + ((lf_normal * u_lf_param.y) / probe_resolution),
+	// Note: Adding 0.5 to shift the position from range (-0.5, 0.5) to (0, 1)
+
+	float3 sample_pos = saturate(lf_position + 0.5) + (lf_normal * u_lf_param.y / probe_resolution);
+
+	// The sampling position is clamped to half a pixel of the tile bounds
+	// to ensure that we stay within out tile, and don't accidentally sample
+	// from a neighbouring tile
+
+	sample_pos = clamp(
+		sample_pos,
 		0.5 / probe_resolution,
 		1.0 - (0.5 / probe_resolution)
 	);
+
+	float3 tile_width = float3(1.0 / 9.0, 0.0, 0.0);
+	sample_pos.x *= tile_width.x;
 
 	factor = min(factor, remainder);
 
 	for(int i = 0; i < 6; i++)
 	{
-		buffer[i] += factor * lf_texture.SampleLevel(
-			lf_sampler,
-			sample_pos
-				* float3(1.0 / 9.0, 1.0, 1.0)
-				+ float3(  i / 9.0, 0.0, 0.0),
-			0.0
-		).x;
+		buffer[i] += factor * lf_texture.SampleLevel(lf_sampler, sample_pos, 0.0).x;
+		sample_pos += tile_width;
 	}
 
 	remainder = max(0, remainder - factor);
@@ -83,7 +92,7 @@ float4 main(
 	float buffer[6] = {0, 0, 0, 0, 0, 0};
 	float remainder = 1.0;
 
-	#define Compute(index) ComputeAmbientValue(index, position, normal, s_LightField##index, SamplerName(s_LightField##index), remainder, buffer)
+	#define Compute(index) ComputeProbeSample(index, position, normal, s_LightField##index, SamplerName(s_LightField##index), remainder, buffer)
 
 	Compute(0);
 	Compute(1);
@@ -106,7 +115,8 @@ float4 main(
 		}
 	}
 
-	if(0.0 < u_lf_param.w)
+	// if u_lf_param.w is set, make AO from the "bottom"/floor be dark
+	if(u_lf_param.w > 0.0)
 	{
 		buffer[3] = 0.0;
 	}
